4b6b948b6e5f1243e57b5cd2f83ffc4fc85d3395
Deneth Weerasinghe <deneth@hotmail.co.uk>
2024-05-19 20:21:02 +0100

    Added RatingEntity

---
+++ data/src/main/java/weerasinghe/deneth/data/RatingEntity.kt
@@ -0,0 +1,13 @@
+package weerasinghe.deneth.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity
+class RatingEntity(
+    @PrimaryKey
+    var id: String = UUID.randomUUID().toString(),
+    var name: String,
+    var description: String
+)
