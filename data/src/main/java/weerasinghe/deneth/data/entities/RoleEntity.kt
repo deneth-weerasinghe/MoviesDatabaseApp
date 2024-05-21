e7063461d863fa1155c88eaf2ac3ecd3bd84efdc
Deneth Weerasinghe <deneth@hotmail.co.uk>
2024-05-21 17:59:30 +0100

    Fixed error in property names

--- data/src/main/java/weerasinghe/deneth/data/entities/RoleEntity.kt
+++ data/src/main/java/weerasinghe/deneth/data/entities/RoleEntity.kt
@@ -8,8 +8,8 @@ import androidx.room.Entity
     primaryKeys = ["actorId", "movieId"]  // needed for associative entities to create unique id
 )
 class RoleEntity(
-    var movieID: String,
-    var actorID: String,
+    var movieId: String,
+    var actorId: String,
     var character: String,  // associative relationship extra data, likewise with below
     var orderInCredits: Int,
 )
