44b72744948fde2a453afad4c3a6848b44e4128b
Deneth Weerasinghe <deneth@hotmail.co.uk>
2024-05-23 14:52:38 +0100

    Added screen property

--- app/src/main/java/weerasinghe/deneth/movies/MovieViewModel.kt
+++ app/src/main/java/weerasinghe/deneth/movies/MovieViewModel.kt
@@ -1,17 +1,51 @@
 package weerasinghe.deneth.movies
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
+import kotlinx.coroutines.launch
 import weerasinghe.deneth.repository.MovieDatabaseRepository
 import weerasinghe.deneth.repository.MovieRepository
+
+// Let's define a few screens
+sealed interface Screen
+// sealed interfaces are interfaces whose implementations are only defined in same module (app module here)
+// useful for exhausting when statements
+object RatingListScreen: Screen
+object MovieListScreen: Screen
+object ActorListScreen: Screen
+
+data class RatingScreen(
+    val id: String
+): Screen
 
 class MovieViewModel(
     private val repository: MovieRepository
 ): ViewModel() {
-// Viewmodel instance exists for as long as the screen/activity is not switched to another
-// Rotating would not create a new viewmodel
+// ViewModel instance exists for as long as the screen/activity is not switched to another
+// Rotating would not create a new ViewModel
 
+    var screen by mutableStateOf<Screen>(RatingListScreen)  // makes it so everytime the screen changes, the function is rerun because of by
+        private set  // public property but hidden setter
+
+    val ratingsFlow = repository.ratingsFlow  // flows of dtos
+    val moviesFlow = repository.moviesFlow
+    val actorsFlow = repository.actorsFlow
+
+    fun switchTo(screen: Screen) {
+        this.screen = screen
+    }
+
+    fun resetDatabase() {  // starts a coroutine and resets database
+        // if this coroutine is running while MovieViewModel ends, it will cancel all the MovieViewModel is doing
+        viewModelScope.launch {
+            repository.resetDatabase()
+        }
+    }
     companion object {
         val Factory: ViewModelProvider.Factory = object: ViewModelProvider.Factory {
             @Suppress("UNCHECKED_CAST")
