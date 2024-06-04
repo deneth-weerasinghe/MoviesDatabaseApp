dc6f85965638ba06a4b0501b266de6a62d00c0b1
Deneth Weerasinghe <deneth@hotmail.co.uk>
2024-06-04 17:42:54 +0100

    Added text overflow handling

--- app/src/main/java/weerasinghe/deneth/movies/components/SimpleText.kt
+++ app/src/main/java/weerasinghe/deneth/movies/components/SimpleText.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -15,6 +16,8 @@ fun SimpleText(
 ) =
     Text(
         text = text,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
         modifier = modifier
             .padding(8.dp)
             .clickable {
